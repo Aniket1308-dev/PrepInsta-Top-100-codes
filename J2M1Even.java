8ea4b2a81a995eceb0fafcd8e50c451f79e6cd7a
Aniket1308-dev <aniketdevadas@gmail.com>
2026-06-14 12:33:13 +0530

    J2 Check Whether a Number is Even or Odd.

---
+++ J2M1Even.java
@@ -0,0 +1,13 @@
+public class J2M1Even {
+
+    public static void main(String[] args) {
+        
+        int num = 28;
+
+        //Checking the number whether its even or odd
+        if (num % 2 == 0)
+            System.out.println("The number is even");
+        else 
+            System.out.println("The number is odd");
+    }
+}

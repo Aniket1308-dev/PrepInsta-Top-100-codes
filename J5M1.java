747c86e527e96595809e24abbbf068e9779dbff0
Aniket1308-dev <aniketdevadas@gmail.com>
2026-06-15 22:37:13 +0530

    J5 Greatest of the two numbers

---
+++ J5M1.java
@@ -0,0 +1,15 @@
+public class J5M1 {
+       public static void main (String[]args)
+   {
+
+     int num1 = 60, num2 = 20;
+     if (num1 == num2)
+       System.out.println ("both are equal");
+     else if (num1 > num2)
+         System.out.println (num1 + " is greater");
+
+     else
+         System.out.println (num2 + " is greater");
+
+   }
+}

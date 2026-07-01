dc0c0a2996773be8a8a6e9a81db3a042a3081cff
Aniket1308-dev <aniketdevadas@gmail.com>
2026-07-01 23:51:48 +0530

    P1J1

--- J1M1Pos.java
+++ J1M1Pos.java
@@ -1,3 +1,18 @@
+// public class J1M1Pos {
+//     public static void main(String[]args)
+//     {
+//         int num = 0;
+
+//         //Conditions to check whether the given number is positive or negative
+//         if(num > 0 )
+//             System.out.println("The number is positive");
+//         else if (num < 0)
+//             System.out.println("The number is negative");
+//         else 
+//             System.out.println("Zero");
+//     }
+// }
+
 public class J1M1Pos {
     public static void main(String[]args)
     {
